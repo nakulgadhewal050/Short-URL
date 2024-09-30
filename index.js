b267987928cf0648f2735d14b1da27f74f6c09d1
Nakul-Gadhewal <nakulgadhewal050@gmail.com>
2024-09-30 16:22:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 const express = require("express");
-
-const app = express();  
-
-const PORT = 8000;
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const { connectToMongoDB } = require("./connect");
+const { checkForAuthentication, restrictTo } = require("./middlewares/auth");
+const URL = require("./models/url");
 
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
+const userRoute = require("./routes/user");
+const router = require("./routes/url");
 
-const {connectToMongoDB} = require("./connect");
+const app = express();
+const PORT = 8001;
 
-const URL = require('./models/url');
 
 
-connectToMongoDB ('mongodb://127.0.0.1:27017/short-url')
-.then(() => console.log("mongodb  connected")
+connectToMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/short-url").then(() =>
+  console.log("Mongodb connected")
 );
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthentication);
 
-app.use('/url', urlRoute);
+app.use("/url", restrictTo(["NORMAL","ADMIN"]) ,urlRoute);
+app.use("/user", userRoute);
+app.use("/",  staticRoute);
 
-app.get('/:shortId', async (req, res) => {
 
-    const shortId = req.params.shortId;
-  const entry = 
-    await URL.findOneAndUpdate({
-        shortId
-    }, { $push: {
-        visitHistory: {
 
-            timestamp: Date.now(),
-
-        },
+app.get("/url/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
     },
-}
- );
-        res.redirect(entry.redirectURL)
-
+    {
+      $push: {
+        visitHistory: {
+          timestamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
 });
 
-app.listen(PORT, () => console.log(`server is started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
